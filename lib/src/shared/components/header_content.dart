8d80268b902f357b7f4abe89fe6271062f6d23b9
messaismael <messaismael01@gmail.com>
2023-11-24 23:16:11 +0100

    fix: begin category card

--- lib/src/shared/components/header_content.dart
+++ lib/src/shared/components/header_content.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
 
 class ContentHeader extends StatelessWidget {
-  const ContentHeader({super.key, required this.title, this.action});
+  const ContentHeader(
+      {super.key, required this.title, required this.child, this.action});
 
   final String title;
+  final Widget child;
   final Widget? action;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(top: 24, bottom: 6),
-      child: Row(
+      child: Column(
         children: [
-          Expanded(
-              child: Text(
-            title,
-            style: Theme.of(context).textTheme.titleSmall,
-          )),
-          if (action != null)
-            Text(
-              title,
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
+          Row(
+            children: [
+              Expanded(
+                  child: Text(
+                title,
+                style: Theme.of(context).textTheme.titleSmall,
+              )),
+              if (action != null) SizedBox(child: action)
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 12.0),
+            child: child,
+          )
         ],
       ),
     );
